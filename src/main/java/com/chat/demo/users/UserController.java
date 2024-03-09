7f8736a1832eb6797f7848b495a74791a1279191
AmimiHamza <amimi1hamza@gmail.com>
2024-03-09 18:57:31 +0100

    feat:allow adding users

--- src/main/java/com/chat/demo/users/UserController.java
+++ src/main/java/com/chat/demo/users/UserController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 
@@ -12,14 +13,45 @@ import org.springframework.ui.Model;
 @AllArgsConstructor
 public class UserController {
     private UserRepo userRepo;
+
     @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/")
+    public String in() {
+        return "index";
+    }
     
+
+    @GetMapping("/users")
     public String users(Model model) {
-        System.out.println("hellooo");
         java.util.List<User> users = userRepo.findAll();
         model.addAttribute("users", users);
         return "users";
     }
+    @GetMapping("/template1")
+    public String navbar() {
+        return "template1";
+    }
+    @GetMapping("/userinfo")
+    public String userinfo() {
+        return "userinfo";
+    }
+
+    @GetMapping("/adduser")
+    public String adduser() {
+        return "adduser";
+    }
+
+    @PostMapping("/save")
+    public String login(Model model,User user,@RequestParam("name") String name,@RequestParam("email") String email, 
+    @RequestParam("password") String password,@RequestParam("role") String role) {
+                            System.out.println("email");
+                            System.out.println(email);
+                            userRepo.save(new User(null,name,email,password,role));
+                            return "index";
+        
     
 
-}
+}}
