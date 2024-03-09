eb01adc2c4b5991464a1408d63eda4dca27f05cc
AmimiHamza <amimi1hamza@gmail.com>
2024-03-09 15:44:26 +0100

    fead:fell in db

--- src/main/java/com/chat/demo/ChatApplication.java
+++ src/main/java/com/chat/demo/ChatApplication.java
@@ -1,7 +1,12 @@
 package com.chat.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.chat.demo.users.User;
+import com.chat.demo.users.UserRepo;
 
 @SpringBootApplication
 public class ChatApplication {
@@ -9,5 +14,17 @@ public class ChatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
 	}
+	@Bean
+    CommandLineRunner commandLineRunner(UserRepo userRepo){
+        return args -> {
+            userRepo.save(new User(null,"hamza","hamimi@insea.ac.ma","hellohamza"));
+            userRepo.save(new User(null,"zakaria","zamimi@insea.ac.ma","hellozakaria"));
+            userRepo.save(new User(null,"ahmed","aamimi@insea.ac.ma","helloahmed"));
+            userRepo.save(new User(null,"mohammed","mamimi@insea.ac.ma","hellomohammed"));
+			userRepo.findAll().forEach(u->{
+				System.out.println(u.getName()+" has been added to the database");
+			} );
+        };
+    }
 
 }
